67179eefb09ee62b85f4201219ff6d6a2830efca
Peter Holzkorn <peter.holzkorn@aec.at>
2016-06-27 17:42:52 +0200

    experiments

--- src/phdemo/core.clj
+++ src/phdemo/core.clj
@@ -33,8 +33,10 @@
 
 (save-stl (mg/seed-box (a/aabb 1 0.5 1))
 
-          (mg/subdiv :cols 3 :rows 3 :out [{} nil {}
-                                           {} nil (mg/scale-edge :bc {:sym :y :scale 0.8})
+          (mg/subdiv :cols 8 :rows 8 :out [{} nil {}
+                                           {} nil (mg/scale-edge :bc {:sym :z :scale 0.8
+                                                                      }
+                                                                 :out [(mg/scale-edge :ad {:sym :y :scale 0.8})])
                                            nil (mg/subdiv :cols 3 :out [nil {} nil
                                                                         ]) nil]
 )
@@ -42,6 +44,51 @@
 
           "foo.stl" 1e6)
 
+
+(save-stl (mg/seed-box (a/aabb 1 1 0.4))
+
+          (mg/subdiv :cols 8 :rows 8 :out (take 64 (repeatedly #(if (> (rand) 0.5)
+
+                                                                  (let [scalef (rand)] (mg/scale-edge :bc :y :scale scalef
+                                                                                                      :out [(mg/scale-edge :bf :x :scale scalef
+                                                                                                                           :out [(mg/subdiv-inset :dir :z :inset 0.4 :out )])]))
+                                                                  nil)))
+)
+
+
+          "foo2.stl" 1e6)
+
+(defn extruder [sc it mx]
+  (if (> it mx)
+    {}
+    (mg/extrude :dir :b :len 0.02 :out [(mg/scale-edge :bc :x :scale sc :out [(mg/subdiv :slices 2 :out [(extruder (Math/sin (* 50. sc)) (inc it) mx)
+                                                                                                         {}])])])))
+
+(defn scale-two-axes [ax1 s1 ax2 s2 sc ou]
+  (mg/scale-edge ax1 s1 :scale sc :out [(mg/scale-edge ax2 s2 :scale sc :out ou)]))
+
+(save-stl (mg/seed-box (a/aabb 1 1 0.8))
+
+          (mg/subdiv :cols 8 :rows 8 :out (take 64 (repeatedly #(if (> (rand) 0.5)
+                                                                  (scale-two-axes :bf :x :bc :y 0.1
+                                                                                  [(scale-two-axes :ad :y :ae :x 0.7
+                                                                                                    [(let [scalef (rand)]
+                                                                                                       (let [d8 (take 8 (repeatedly rand))]
+                                                                                                         (mg/subdiv :slices 8
+                                                                                                                    :out (map (fn [i] (let [ld (nth d8 i)
+                                                                                                                                           ud (nth d8 (+ 1 i))]
+                                                                                                                                       (scale-two-axes :bc :y :bf :x ud [(scale-two-axes :ad :y :ae :x ld [{}])])
+                                                                                                                                       #_(mg/scale-edge :bc :y :scale ud :out [(mg/scale-edge :ad :y :scale ld)])))
+                                                                                                                              (range 7)))))])])
+                                                                  nil)))
+)
+
+
+          "foo3.stl" 1e6)
+
+(take 64 (repeat {}))
+
+(mapv)
 
 (defn save-stl [seed tree path max-depth]
   (with-open [o (io/output-stream path)]
